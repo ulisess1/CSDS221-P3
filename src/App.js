d41f58a0f26034a2d8d699e80400a07d74d91ff4
Ulises Soto <uxs58@case.edu>
2023-04-28 23:30:48 -0400

    final changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { useState } from 'react';
-import Board from './Board'
+import Board from './Board';
+import { Grid, Dialog, Button, Typography } from '@mui/material';
+import "./app.css";
 
-export default function App() {
-  const [clear, setClear] = useState(false); //determine if game should be reset
-  const [winner, setWinner] = useState(''); //winning player
+function App() {
+  const [reset, setReset] = useState(false);
+  const [winner, setWinner] = useState('');
+  const [p1Wins, setP1Wins] = useState(0);
+  const [p2Wins, setP2Wins] = useState(0);
+  const [turn, setTurn] = useState(0);
 
-  const clearBoard = () => {
-    setClear(true);
-  };
+  const setOpen = () => {
+    return winner !== '';
+  }
+
+  const resetBoard = () => {
+    setReset(true);
+    setOpen(false);
+    if (turn === 0) {
+      setP2Wins(p2Wins + 1);
+    } else {
+      setP1Wins(p1Wins + 1);
+    }
+  }
 
   return (
-    <div>
-      <div>
-        <div>{winner} has WON!</div>
-        <button onClick={() => clearBoard()}>Reset Game</button>
+    <div className='App'>
+      <Typography sx={{ fontSize: '3.0rem', fontFamily: 'Verdana, Geneva, Tahoma, sans-serif', color: '#ffb6c1', textShadow: '2px 2px 0px #f00' }} className='retro-title'>Tic Tac Toe</Typography>
+      <Typography sx={{ fontFamily: 'Courier New, Courier, monospace', color: 'black', textShadow: '1px 1px 0px #000' }}>
+        <span>Player 1:</span> <span style={{ color: 'black' }}>X</span><br />
+        <span>Player 2:</span> <span style={{ color: 'black' }}>O</span><br />
+      </Typography>
+
+
+      <Board reset={reset} setReset={setReset} winner={winner}
+        setWinner={setWinner} setOpen={setOpen} p1Wins={p1Wins} turn={turn} setTurn={setTurn} />
+
+      <div class="container">
+        <div class="wins">Wins</div>
+        <div class="players">
+          <div class="player">
+            <div class="title">Player 1:</div>
+            <div class="count">{p1Wins}</div>
+          </div>
+          <div class="player">
+            <div class="title">Player 2:</div>
+            <div class="count">{p2Wins}</div>
+          </div>
+        </div>
       </div>
-      {/* <Board/> */}
+
+
+      <Dialog sx={{ textAlign: 'center' }} open={setOpen()} maxWidth="xs" fullWidth={true}>
+        <h2>Game Over</h2>
+        <h3>{winner} <br /></h3>
+        <Button onClick={() => resetBoard()}>Play Again!</Button>
+      </Dialog>
     </div>
+
   );
 }
+
+export default App;
--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,145 @@
-import { useState, useEffect, useRef } from 'react';
+import "./board.css";
+import React, { useState, useEffect, useRef } from "react";
 
-const Board = () => {
-  const [turn, setTurn] = useState(0); //track player turn, either 0 or 1
-  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']); //keep track of board
+const Board = ({ reset, setReset, winner, setWinner, turn, setTurn }) => {
+    const emptyBoard = ['', '', '', '', '', '', '', '', ''];
 
-  return (
-    <div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-      <div onClick={alert('test')}></div>
-    </div>
-  );
-};
+	// Creating a turn state, which indicates the current turn
+	const [data, setData] = useState(emptyBoard)
+
+	// Creating a reference for the board
+	const boardRef = useRef(null);
+
+	// Function to draw on the board
+	const draw = (event, index) => {
+		// Draws only if the position is not taken
+		// and winner is not decided yet
+		if (data[index - 1] === '' && winner === '') {
+
+			// Draws X if it's player 1's turn else draws O
+			const current = turn === 0 ? "X" : "O"
+
+			// Updating the data state
+			data[index - 1] = current;
+
+			//Drawing on the board
+			event.target.innerText = current;
+
+			// Switching the turn
+			setTurn(turn === 0 ? 1 : 0)
+		}
+	}
+
+	// UseEffect hook used to reset the board whenever
+	// a winner is decided
+	useEffect(() => {
+
+		// Clearing the data state
+		setData(emptyBoard);
+
+		// Getting all the children(cells) of the board
+		const cells = boardRef.current.children
+
+		// Clearing out the board
+		for (let i = 0; i < 9; i++) {
+			cells[i].innerText = '';
+		}
+
+		// Resetting the turn to player 0
+		setTurn(0);
+
+		// Resetting the winner
+		setWinner('');
+		setReset(false);
+	}, [reset, setReset, setWinner])
+
+
+	// useEffect hook used to check for a winner
+	useEffect(() => {
+
+		// Checks for the win condition in rows
+		const checkRow = () => {
+			let ans = false;
+			for (let i = 0; i < 9; i += 3) {
+				ans |= (data[i] === data[i + 1] &&
+				data[i] === data[i + 2] &&
+				data[i] !== '')
+			}
+			return ans;
+		}
+
+		// Checks for the win condition in cols
+		const checkCol = () => {
+			let ans = false;
+			for (let i = 0; i < 3; i++) {
+				ans |= (data[i] === data[i + 3] &&
+				data[i] === data[i + 6] &&
+				data[i] !== '')
+			}
+			return ans;
+		}
+
+		// Checks for the win condition in diagonals
+		const checkDiagonal = () => {
+			return ((data[0] === data[4] &&
+			data[0] === data[8] && data[0] !== '') ||
+			(data[2] === data[4] && data[2] === data[6] &&
+			data[2] !== ''));
+		}
+
+		// Checks if at all a win condition is present
+		const checkWin = () => {
+			return (checkRow() || checkCol() || checkDiagonal());
+		}
+
+		// Checks for a tie
+		const checkTie = () => {
+			let count = 0;
+			data.forEach((cell) => {
+				if (cell !== '') {
+					count++;
+				}
+			})
+			return count === 9;
+		}
+
+		// Setting the winner in case of a win
+		if (checkWin()) {
+            if (turn === 0) {
+                setWinner("Player 2 Wins!");
+            }
+            else {
+                setWinner("Player 1 Wins!");
+            }
+		} else if (checkTie()) {
+			// Setting the winner to tie in case of a tie
+			setWinner("It's a Tie!");
+		}
+
+	})
+
+	return (
+		<div ref={boardRef} className="board">
+			<div className="square square-1"
+				onClick={(e) => draw(e, 1)}></div>
+			<div className="square square-2"
+				onClick={(e) => draw(e, 2)}></div>
+			<div className="square square-3"
+				onClick={(e) => draw(e, 3)}></div>
+			<div className="square square-4"
+				onClick={(e) => draw(e, 4)}></div>
+			<div className="square square-5"
+				onClick={(e) => draw(e, 5)}></div>
+			<div className="square square-6"
+				onClick={(e) => draw(e, 6)}></div>
+			<div className="square square-7"
+				onClick={(e) => draw(e, 7)}></div>
+			<div className="square square-8"
+				onClick={(e) => draw(e, 8)}></div>
+			<div className="square square-9"
+				onClick={(e) => draw(e, 9)}></div>
+		</div>
+	)
+}
 
 export default Board;
